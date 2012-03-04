87f03a3abb4c53f2f7af2267589d41812819d352
Eric Benner <xeology@aesircraft.net>
2012-03-03 20:10:01 -0500

    Fixed Creeper Exploding Machine blocks.

--- src/net/aesircraft/VisCraft/Listeners/CreatureListener.java
+++ src/net/aesircraft/VisCraft/Listeners/CreatureListener.java
@@ -1,15 +1,18 @@
 package net.aesircraft.VisCraft.Listeners;
 
+import java.util.List;
 import net.aesircraft.VisCraft.Data.Machines;
 import net.aesircraft.VisCraft.Data.VisLocation;
 import net.aesircraft.VisCraft.VisCraft;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class CreatureListener implements Listener {
 
@@ -34,6 +37,20 @@ public class CreatureListener implements Listener {
                 event.setCancelled(false);
                 Machines.safeSpawns.remove(event.getEntity().getEntityId());
             }
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEntityExplode(final EntityExplodeEvent event) {
+        if (!event.isCancelled()) {
+            List<Block> blocks=event.blockList();
+            VisLocation l=new VisLocation();
+            for (Block b : blocks){
+                l.loadFromLocation(b.getLocation());
+                if (Machines.protectedBlocks.containsKey(l.toString()) || Machines.signBlocks.contains(l.toString())){
+                    blocks.remove(b);
+                }
+            }            
         }
     }
 }
